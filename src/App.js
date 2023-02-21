b7154473a49d05a15231f619ad4086b223fb8838
Tyler Hawkins <tylerh@adobe.com>
2023-02-21 10:19:33 -0700

    feat: adds React concept examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const someCondition = false;
+  const shoppingListItems = ['Bread', 'Milk', 'Eggs'];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>New text here {2 + 2}</p>
+        {someCondition && (<p>The condition was true</p>)}
+        {someCondition ? <p>The condition was true</p> : <p>The condition was false</p>}
+
+        <Counter />
+        <Counter />
+
+        <ol>
+          <li>Bread</li>
+          <li>Milk</li>
+          <li>Eggs</li>
+        </ol>
+
+        <ol>
+          {shoppingListItems.map(item => <li key={item}>{item}</li>)}
+        </ol>
+
+        <ShoppingList items={shoppingListItems} />
+        <ShoppingList items={['Oreos', 'Gummy Worms']} />
       </header>
     </div>
   );
+}
+
+function ShoppingList(props) {
+  return (
+    <>
+      <h2>My shopping list</h2>
+      <ol>
+        {props.items.map(item => <li key={item}>{item}</li>)}
+      </ol>
+    </>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count => count + 1);
+  }
+
+  return (
+    <>
+      <h2>My Counter</h2>
+      <p>Count: {count}</p>
+      <button type="button" disabled={count >= 10} onClick={incrementCounter}>Increment Counter</button>
+    </>
+  )
 }
 
 export default App;
